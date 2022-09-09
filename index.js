bd8e40dd461b72deaa6e780564bb244f10559942
Keshav Bhati <keshavbhati2000@gmail.com>
2022-09-09 15:05:05 -0400

    commit tree 524992e0166f9376819d70bca5a278581c72fd4a

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+function beforeEach(){
+    cats.splice(0);
+    cats.push("Milo", "Otis", "Garfield");
+};
+function destructivelyAppendCat(name){
+    beforeEach();
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    beforeEach();
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    beforeEach();
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    beforeEach();
+    cats.shift();
+}
+function appendCat(name){
+    beforeEach();
+    const newcats=[...cats, name];
+    return newcats;
+}
+function prependCat(name){
+    beforeEach();
+    const newcats=[name, ...cats];
+    return newcats;
+}
+function removeLastCat(){
+    beforeEach();
+    const newcats = cats.slice(0,-1);
+    return newcats; 
+}
+function removeFirstCat(){
+    beforeEach();
+    const newcats = cats.slice(1, cats.length);
+    return newcats;
+}
